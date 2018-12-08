51667f3755691e7ba87add58069cd63402a43dd3
abentertainment <argertboja@yahoo.com>
2018-12-08 04:01:21 +0300

    Character motion and Skybox Design
    * Created the path that will be followed by each character.
    * Created the script which makes the characters follow the waypoints and look towards them.
    * Designed the night feeleing of environment

---
+++ Assets/Scripts/PlayerMotionIntro.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotionIntro : MonoBehaviour {
+
+    // Variables
+    [SerializeField]
+    private Transform[] targets; //Holds the waypoints
+    [SerializeField]
+    private float speed; //moving speed of character
+    [SerializeField]
+    private float rotationSpeed; //rotation speed toward waypoint
+
+    private int current;
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position != targets[current].position) {
+            // Move to next waypoint and look at it
+            Vector3 newPosition = Vector3.MoveTowards(transform.position, targets[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(newPosition);
+            //Find direction where to look at
+            Vector3 direction = targets[current].position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        }
+        else current = current + 1;
+	}
+}
